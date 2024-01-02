353609a9108eeb6cea8c5cbf9b32f23977af522c
João Manoel <joaomanoeldev@gmail.com>
2024-01-02 02:55:55 -0300

    feat: create auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,13 @@
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
