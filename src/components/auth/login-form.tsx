1fddbd18493d224b15cb999c8de4026a62610ef2
João Manoel <joaomanoeldev@gmail.com>
2024-01-03 23:01:45 -0300

    feat: implementing form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,6 +1,38 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { LoginInput, loginSchema } from "@/schemas"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
 import { CardWrapper } from "@/components/auth/card-wrapper"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { FormError } from "@/components/form-error"
+import { FormSuccess } from "@/components/form-success"
 
 export const LoginForm = () => {
+  const loginForm = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  })
+
+  const onSubmit = (data: LoginInput) => {
+    console.log("[DATA]", data)
+    loginForm.reset()
+  }
+
   return (
     <CardWrapper
       headerLabel="Bem-vindo de volta"
@@ -8,7 +40,44 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form
+      <Form {...loginForm}>
+        <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="email@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormError message="" />
+          <FormSuccess message="" />
+
+          <Button className="w-full" type="submit">Entrar</Button>
+        </form>
+      </Form>
     </CardWrapper>
   )
 }
