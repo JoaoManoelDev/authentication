0f090c180c48771c9e969df52b0c44f5691c27f8
João Manoel <joaomanoeldev@gmail.com>
2024-01-04 02:49:03 -0300

    feat: create register schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,3 +6,11 @@ export const loginSchema = z.object({
 })
 
 export type LoginInput = z.infer<typeof loginSchema>
+
+export const registerSchema = z.object({
+  email: z.string().min(1, "O Email não pode estar vazio.").email("Digite um Email válido."),
+  password: z.string().min(6, "A senha deve conter no mínimo 6 caracteres."),
+  name: z.string().min(3, "O nome deve conter no mínimo 3 caracteres.")
+})
+
+export type RegisterInput = z.infer<typeof registerSchema>
