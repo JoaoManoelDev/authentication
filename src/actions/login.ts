30ba7d45da554a50ed3af93020ccb94c74a3a518
João Manoel <joaomanoeldev@gmail.com>
2024-01-06 13:29:11 -0300

    feat: implementing login

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,9 @@
 "use server"
 
+import { signIn } from "@/auth"
 import { LoginInput, loginSchema } from "@/schemas"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { AuthError } from "next-auth"
 
 export const login = async (data: LoginInput) => {
   const validateFields = loginSchema.safeParse(data)
@@ -9,5 +12,24 @@ export const login = async (data: LoginInput) => {
     return { error: "Campo inválido" }
   }
 
-  return { success: "Tudo certo" }
+  const { email, password } = validateFields.data
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch(error.type) {
+        case "CredentialsSignin":
+          return { error: "Email ou senha incorretos." }
+        default:
+          return { error: "Algo deu errado." }
+      }
+    }
+
+    throw error
+  }
 }
