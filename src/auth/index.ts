cdd7e1c35c8f3e104117d7b0a90a12b74ee8d5fd
João Manoel <joaomanoeldev@gmail.com>
2024-01-15 23:49:02 -0300

    chore: move next auth config to auth folder

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "@/lib/db"
-import authConfig from "@/auth.config"
+import authConfig from "./auth.config"
 
 export const {
   handlers: { GET, POST },
