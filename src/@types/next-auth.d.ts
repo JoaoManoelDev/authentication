11323b78968bad9f60445f1acf155a414eaa524b
João Manoel <joaomanoeldev@gmail.com>
2024-01-18 19:56:18 -0300

    chore: add user and admin role types

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import NextAuth, { type DefaultSession } from "next-auth"
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "user" | "admin"
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser
+  }
+}
