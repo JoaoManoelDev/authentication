7388f7ad7cf6ad6ff593a57c6dfcc67016c8b012
João Manoel <joaomanoeldev@gmail.com>
2024-01-19 23:42:42 -0300

    feat: create signIn function

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,16 +1,25 @@
 "use client"
 
+import { signIn } from "next-auth/react"
+
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export const Social = () => {
+  const onClick = (provider: "github" | "google") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    })
+  }
+
   return (
     <div className="flex items-center w-full gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <Icons.google className="w-5 h-5" />
       </Button>
@@ -19,7 +28,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <Icons.gitHub className="w-5 h-5" />
       </Button>
