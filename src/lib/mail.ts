661a19602756e1535ae2439255732c921a13c747
João Manoel <joaomanoeldev@gmail.com>
2024-01-23 16:33:29 -0300

    feat: creating send verification email using resend

---
+++ src/lib/mail.ts
@@ -0,0 +1,14 @@
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Confirme seu email",
+    html: `<p> Clique <a href="${confirmLink}">aqui</a> para confirmar email </p>`
+  })
+}
